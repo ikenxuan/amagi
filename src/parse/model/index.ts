f4907391c79792eacf71f712656d3b142a6dda84
ikenxuan <2066855608@qq.com>
2024-07-19 00:57:32 +0800

    fix: 修改日志呈现形式

--- src/parse/model/index.ts
+++ src/parse/model/index.ts
@@ -1,23 +1,5 @@
 import networks from './networks';
 import cfg from './config';
-import log4js from 'log4js';
-
-
-log4js.configure({
-  appenders: {
-    console: {
-      type: 'stdout',
-      layout: {
-        type: 'pattern',
-        pattern: '[%[%d{yyyy-MM-dd hh:mm:ss}] [%p] %m%]'
-      }
-    }
-  },
-  categories: {
-    default: { appenders: ['console'], level: 'debug' },
-  },
-});
-
-const logger = log4js.getLogger();
+import { logger } from './logger';
 
 export { networks, cfg, logger }
