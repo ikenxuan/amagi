722782267192b8ced3cf4f8f6aa9b6351ab95e00
ikenxuan <2066855608@qq.com>
2024-07-18 22:55:22 +0800

    fix: 编译过程中会擦除配置文件的问题

--- src/cpab.js
+++ src/cpab.js
@@ -1,24 +1,28 @@
 import fs from 'fs'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import chalk from 'chalk'
 
 const sourcePath = path.join(dirname(fileURLToPath(import.meta.url)), 'parse', 'sign', 'a_bougs.cjs')
 const targetPath = path.join(dirname(fileURLToPath(import.meta.url)), '..', 'lib', 'parse', 'sign', 'a_bougs.cjs')
 fs.copyFileSync(sourcePath, targetPath)
 
+const configDir = path.join(process.cwd(), 'config')
+const configFilePath = path.join(configDir, 'config.yaml')
+
 function createcfgfile () {
-  const configDir = path.join(process.cwd(), 'config')
-  const configFilePath = path.join(configDir, 'config.yaml')
   if (!fs.existsSync(configDir)) {
     fs.mkdirSync(configDir, { recursive: true })
   }
 
-  const configContent = `
-ck: 
-  `
+  const configContent = `ck: `
 
   fs.writeFileSync(configFilePath, configContent)
 }
-try { fs.readFileSync(configFilePath) } catch { createcfgfile() }
+try {
+  fs.readFileSync(configFilePath)
+} catch {
+  createcfgfile()
+}
 
-console.log('Done, The build is success!')
+console.log(chalk.green('Done, The build is success!'))
