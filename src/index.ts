6e113beef5580293a1f18b883b073776a66698c5
ikenxuan <2066855608@qq.com>
2024-09-23 01:23:15 +0800

    refactor(server): 更新客户端方法并标记为废弃
    - 重新组织`client`类并将其方法标记为废弃，推荐使用更新的导入方法。
    - 在`index.ts`中导入并导出`amagi`客户端。
    - 将数据抓取方法迁移至`DataFetchers.ts`中，并提供新方法的类型注解。
    - 更新`client.ts`中的方法引用，并添加对应的新导入。
    
    BREAKING CHANGE: 旧的`GetDouyinData`、`GetBilibiliData`和`GetXiaohongshuData`方法已被废弃，
    请使用小写的`getDouyinData`、`getBilibiliData`和`getXiaohongshuData`方法。

--- src/index.ts
+++ src/index.ts
@@ -2,50 +2,6 @@ export * from "amagi/business"
 export * from "amagi/model"
 export * from "amagi/server"
 export * from "amagi/types"
+
+import { amagi } from "amagi/server"
 export { amagi as Amagi, amagi as default }
-import { DouyinResult, BilibiliResult, XiaohongshuResult } from 'amagi/business'
-import { client } from "amagi/server"
-import { DouyinDataOptionsMap, BilibiliDataOptionsMap, XiaohongshuDataOptionsMap } from 'amagi/types'
-
-const amagi = client
-
-/**
- * 
- * @param type 请求数据类型
- * @param cookie 抖音用户 ck
- * @param options 请求参数，是一个对象
- * @returns 返回接口的原始数据
- */
-export const GetDouyinData = async <T extends keyof DouyinDataOptionsMap> (
-  type: T,
-  cookie = '' as string,
-  options: DouyinDataOptionsMap[T]
-): Promise<any> => {
-  const data = await DouyinResult({ type, cookie }, options)
-  return data.data
-}
-
-/**
- * 
- * @param type 请求数据类型
- * @param cookie bilibili 用户 ck
- * @param options 请求参数，是一个对象
- * @returns 返回接口的原始数据
- */
-export const GetBilibiliData = async <T extends keyof BilibiliDataOptionsMap> (
-  type: T,
-  cookie = '' as string,
-  options: BilibiliDataOptionsMap[T]
-): Promise<any> => {
-  const data = await BilibiliResult({ type, cookie }, options)
-  return data.data
-}
-
-export const GetXiaohongshuData = async <T extends keyof XiaohongshuDataOptionsMap> (
-  type: T,
-  cookie = '' as string,
-  options: XiaohongshuDataOptionsMap[T]
-): Promise<any> => {
-  const data = await XiaohongshuResult({ type, cookie }, options)
-  return data.data
-}
--- src/server/listen.ts
+++ src/server/listen.ts
@@ -7,9 +7,15 @@ import { FastifyInstance } from 'fastify'
  * @param port 监听端口
  * @returns
  */
-export const StartClient = async (client: FastifyInstance, port: 4567): Promise<void> => {
+export const startClient = async (client: FastifyInstance, port: 4567): Promise<void> => {
   return client.listen({ port: port, host: '::' }, (_err, _address) => {
     if (_err) client.log.error(_err)
     logger.mark(`amagi server listening on ${port} port. API docs: https://amagi.apifox.cn`)
   })
 }
+
+/**
+ * 已废弃，请使用 startClient 方法
+ * @deprecated
+ */
+export const StartClient = startClient
