56b256b5e6280061dfb0d314fcd847ad3577a46e
炫炫 <2066855608@qq.com>
2025-01-08 03:36:38 +0800

    fix

--- src/model/tools.ts
+++ src/model/tools.ts
@@ -7,11 +7,17 @@ import { logger } from './logger'
  * @param atLeastOne - 是否至少有一个字段存在即可通过检查，默认为false
  */
 export const validateData = (data: { methodType: string } & Record<string, any>, fields: string[], atLeastOne = false): void => {
-  const missingFields = atLeastOne
-    ? fields.filter(field => !data.hasOwnProperty(field))
-    : fields.filter(field => data.hasOwnProperty(field))
+  const checkField = (field: string) => Object.prototype.hasOwnProperty.call(data, field)
 
-  if (missingFields.length) {
+  const isValid = atLeastOne
+    ? fields.some(checkField) // 至少一个字段存在
+    : fields.every(checkField) // 所有字段都必须存在
+
+  if (isValid === false) {
+    const missingFields = atLeastOne
+      ? fields // 如果是至少一个，则提示所有字段
+      : fields.filter(field => !checkField(field)) // 否则只提示缺失的字段
+
     const missingFieldsString = missingFields.map(field => `'${logger.green(field)}'`).join(', ')
     throw new Error(`获取「${data.methodType}」${logger.red('缺少必要的参数')}: ${missingFieldsString}`)
   }
