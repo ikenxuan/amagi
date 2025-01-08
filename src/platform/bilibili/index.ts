b0f845f58805856b7aec96fb7ad77f8322f216ae
炫炫 <2066855608@qq.com>
2025-01-08 16:23:09 +0800

    细优

--- src/platform/bilibili/index.ts
+++ src/platform/bilibili/index.ts
@@ -1,8 +1,7 @@
 import { bilibiliAPI } from './API'
 import { BilibiliData } from './getdata'
-import { getBilibiliID } from './getid'
 import { qtparam } from './qtparam'
 import { wbi_sign } from './sign/wbi'
 
 export { av2bv, bv2av } from './sign/bv2av'
-export { bilibiliAPI, BilibiliData, getBilibiliID, qtparam, wbi_sign }
+export { bilibiliAPI, BilibiliData, qtparam, wbi_sign }
--- src/platform/douyin/index.ts
+++ src/platform/douyin/index.ts
@@ -1,6 +1,5 @@
 import { douyinAPI } from './API'
 import { DouyinData } from './getdata'
-import { getDouyinID } from './getid'
 import douyinSign from './sign'
 
-export { douyinAPI, DouyinData, douyinSign, getDouyinID }
+export { douyinAPI, DouyinData, douyinSign }
