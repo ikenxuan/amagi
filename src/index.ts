0afc7aa8d55b6057caf7f0b93002a4b40ff4e2ae
炫炫 <2066855608@qq.com>
2025-05-16 04:56:07 +0800

    fix: 优化默认导出类型

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,39 @@ import {
 type AmagiConstructor = {
   new(options: cookiesOptions): amagiClient
   (options: cookiesOptions): amagiClient
+  /** 抖音相关功能模块 (工具集) */
+  douyin: typeof douyinUtils
+  /** B站相关功能模块 (工具集) */
+  bilibili: typeof bilibiliUtils
+  /** 快手相关功能模块 (工具集) */
+  kuaishou: typeof kuaishouUtils
+
+  /**
+   * 快捷获取抖音数据
+   * @param type - 请求数据类型
+   * @param cookie - 有效的用户Cookie
+   * @param options - 请求参数，是一个对象
+   * @returns 返回接口的原始数据
+   */
+  getDouyinData: () => typeof douyinUtils.getDouyinData
+
+  /**
+   * 快捷获取B站数据
+   * @param type - 请求数据类型
+   * @param cookie - 有效的用户Cookie
+   * @param options - 请求参数，是一个对象
+   * @returns 返回接口的原始数据
+   */
+  getBilibiliData: () => typeof bilibiliUtils.getBilibiliData
+
+  /**
+   * 快捷获取快手数据
+   * @param type - 请求数据类型
+   * @param cookie - 有效的用户Cookie
+   * @param options - 请求参数，是一个对象
+   * @returns 返回接口的原始数据
+   */
+  getKuaishouData: () => typeof kuaishouUtils.getKuaishouData
 }
 
 /**
@@ -22,27 +55,25 @@ type AmagiConstructor = {
  * @param options - cookies 配置选项，用于设置客户端的 cookies 相关参数
  * @returns 返回一个新的 amagi 客户端实例
  */
-function createAmagiClient (this: void | AmagiConstructor, options: cookiesOptions = {}): amagiClient {
+function createAmagiClient (this: AmagiConstructor, options: cookiesOptions = {}): amagiClient {
   // 是否通过 new 关键字调用
   if (!(this instanceof createAmagiClient)) {
-    return new (createAmagiClient as AmagiConstructor)(options)
+    return new (createAmagiClient as unknown as new (options: cookiesOptions) => amagiClient)(options)
   }
 
   return new amagiClient(options)
 }
 
+createAmagiClient.douyin = douyinUtils
+createAmagiClient.bilibili = bilibiliUtils
+createAmagiClient.kuaishou = kuaishouUtils
+
+createAmagiClient.getDouyinData = () => douyinUtils.getDouyinData
+createAmagiClient.getBilibiliData = () => bilibiliUtils.getBilibiliData
+createAmagiClient.getKuaishouData = () => kuaishouUtils.getKuaishouData
+
 /** After instantiation, it can interact with the specified platform API to quickly obtain data. */
-export const CreateApp = Object.assign(
-  createAmagiClient as AmagiConstructor,
-  {
-    /** 抖音相关功能模块 (工具集) */
-    douyin: douyinUtils,
-    /** B站相关功能模块 (工具集) */
-    bilibili: bilibiliUtils,
-    /** 快手相关功能模块 (工具集) */
-    kuaishou: kuaishouUtils
-  }
-)
+export const CreateApp = createAmagiClient as AmagiConstructor
 
 /** After instantiation, it can interact with the specified platform API to quickly obtain data. */
 const Client = CreateApp
