077714728275a357052e32783b48ec7c3065a2e8
炫炫 <2066855608@qq.com>
2025-11-21 22:30:10 +0800

    fix: 细优

--- src/platform/defaultConfigs.ts
+++ src/platform/defaultConfigs.ts
@@ -61,19 +61,18 @@ export const getDouyinDefaultConfig = (cookie?: string, requestConfig?: RequestC
  */
 export const getBilibiliDefaultConfig = (cookie?: string, requestConfig?: RequestConfig): AxiosRequestConfig => {
   const defHeaders: RequestConfig['headers'] = {
-    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    Accept: 'application/json, text/plain, */*',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'Cache-Control': 'max-age=0',
-    Priority: 'u=0, i',
+    'Accept-Encoding': 'gzip, deflate, br',
+    Origin: 'https://www.bilibili.com',
+    Referer: 'https://www.bilibili.com/',
+    Priority: 'u=1, i',
     'Sec-Ch-Ua': '\"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"',
     'Sec-Ch-Ua-Mobile': '?0',
     'Sec-Ch-Ua-Platform': '\"Windows\"',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-    Referer: 'https://www.bilibili.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
     Cookie: cookie ? cookie.replace(/\s+/g, '') : ''
   }
 
