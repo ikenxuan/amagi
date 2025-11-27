807e236b1145121acd61052c8e6ee3fe9351019e
炫炫 <2066855608@qq.com>
2025-11-27 20:23:20 +0800

    refactor(bilibili): Restructure dynamic info types with discriminated union
    - Move DynamicType enum to top of file for better visibility
    - Add BiliDynamicInfoUnion as discriminated union type for direct type inference
    - Simplify DataData type by removing unnecessary property spread
    - Reorder type definitions to improve code organization and readability
    - Update union type to use direct dynamic type references instead of generic wrapper
    - Add comprehensive JSDoc with usage examples for discriminated union pattern
    - Preserve generic BiliDynamicInfo type for internal use cases

--- src/types/ReturnDataType/Bilibili/DynamicInfo.ts
+++ src/types/ReturnDataType/Bilibili/DynamicInfo.ts
@@ -1,21 +1,5 @@
 import { DynamicTypeArticle, DynamicTypeAV, DynamicTypeDraw, DynamicTypeForwardUnion, DynamicTypeLiveRcmd, DynamicTypeWord } from './index'
 
-type AVItem = DynamicTypeAV['data']['item']
-type DrawItem = DynamicTypeDraw['data']['item']
-type WordItem = DynamicTypeWord['data']['item']
-type LiveRcmdItem = DynamicTypeLiveRcmd['data']['item']
-type ForwardItem = DynamicTypeForwardUnion['data']['item']
-type ArticleItem = DynamicTypeArticle['data']['item']
-
-type DynamicTypeItemMap = {
-  [DynamicType.AV]: AVItem
-  [DynamicType.DRAW]: DrawItem
-  [DynamicType.WORD]: WordItem
-  [DynamicType.LIVE_RCMD]: LiveRcmdItem
-  [DynamicType.FORWARD]: ForwardItem
-  [DynamicType.ARTICLE]: ArticleItem['data']['item']
-}
-
 export enum DynamicType {
   AV = 'DYNAMIC_TYPE_AV',
   DRAW = 'DYNAMIC_TYPE_DRAW',
@@ -25,9 +9,25 @@ export enum DynamicType {
   ARTICLE = 'DYNAMIC_TYPE_ARTICLE'
 }
 
+export type BiliDynamicInfoUnion =
+  | DynamicTypeAV
+  | DynamicTypeDraw
+  | DynamicTypeWord
+  | DynamicTypeLiveRcmd
+  | DynamicTypeForwardUnion
+  | DynamicTypeArticle
+
+type DynamicTypeItemMap = {
+  [DynamicType.AV]: DynamicTypeAV['data']['item']
+  [DynamicType.DRAW]: DynamicTypeDraw['data']['item']
+  [DynamicType.WORD]: DynamicTypeWord['data']['item']
+  [DynamicType.LIVE_RCMD]: DynamicTypeLiveRcmd['data']['item']
+  [DynamicType.FORWARD]: DynamicTypeForwardUnion['data']['item']
+  [DynamicType.ARTICLE]: DynamicTypeArticle['data']['item']
+}
+
 type DataData<T extends DynamicType> = {
-  item: DynamicTypeItemMap[T];
-  [property: string]: any
+  item: DynamicTypeItemMap[T]
 }
 
 export type BiliDynamicInfo<T extends DynamicType> = {
@@ -37,11 +37,3 @@ export type BiliDynamicInfo<T extends DynamicType> = {
   ttl: number;
   [property: string]: any
 }
-
-export type BiliDynamicInfoUnion =
-  | BiliDynamicInfo<DynamicType.AV>
-  | BiliDynamicInfo<DynamicType.DRAW>
-  | BiliDynamicInfo<DynamicType.WORD>
-  | BiliDynamicInfo<DynamicType.LIVE_RCMD>
-  | BiliDynamicInfo<DynamicType.FORWARD>
-  | BiliDynamicInfo<DynamicType.ARTICLE>
