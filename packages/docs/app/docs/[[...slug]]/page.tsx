c9996cce8d9f0f55b14a065a89633a6134bc1e17
炫炫 <2066855608@qq.com>
2026-01-11 15:15:56 +0800

    docs: update

--- packages/docs/app/docs/[[...slug]]/page.tsx
+++ packages/docs/app/docs/[[...slug]]/page.tsx
@@ -1,25 +1,31 @@
-import { getPageImage, source } from '@/lib/source';
-import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/layouts/docs/page';
-import { notFound } from 'next/navigation';
-import { getMDXComponents } from '@/mdx-components';
-import type { Metadata } from 'next';
-import { createRelativeLink } from 'fumadocs-ui/mdx';
-import { LLMCopyButton, ViewOptions } from '@/components/ai/page-actions';
+import { getPageImage, source } from '@/lib/source'
+import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/layouts/docs/page'
+import { notFound } from 'next/navigation'
+import { getMDXComponents } from '@/mdx-components'
+import type { Metadata } from 'next'
+import { createRelativeLink } from 'fumadocs-ui/mdx'
+import { LLMCopyButton, ViewOptions } from '@/components/ai/page-actions'
 
-export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export default async function Page (props: PageProps<'/docs/[[...slug]]'>) {
+  const params = await props.params
+  const page = source.getPage(params.slug)
+  if (!page) notFound()
 
-  const MDX = page.data.body;
+  const MDX = page.data.body
   const gitConfig = {
-    user: 'username',
-    repo: 'repo',
+    user: 'ikenxuan',
+    repo: 'amagi',
     branch: 'main',
-  };
+  }
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      tableOfContent={{
+        style: 'clerk',
+      }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription className="mb-0">{page.data.description}</DocsDescription>
       <div className="flex flex-row gap-2 items-center border-b pb-6">
@@ -27,7 +33,7 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
         <ViewOptions
           markdownUrl={`${page.url}.mdx`}
           // update it to match your repo
-          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/docs/content/docs/${page.path}`}
+          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/packages/docs/content/docs/${page.path}`}
         />
       </div>
       <DocsBody>
@@ -39,17 +45,17 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
         />
       </DocsBody>
     </DocsPage>
-  );
+  )
 }
 
-export async function generateStaticParams() {
-  return source.generateParams();
+export async function generateStaticParams () {
+  return source.generateParams()
 }
 
-export async function generateMetadata(props: PageProps<'/docs/[[...slug]]'>): Promise<Metadata> {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export async function generateMetadata (props: PageProps<'/docs/[[...slug]]'>): Promise<Metadata> {
+  const params = await props.params
+  const page = source.getPage(params.slug)
+  if (!page) notFound()
 
   return {
     title: page.data.title,
@@ -57,5 +63,5 @@ export async function generateMetadata(props: PageProps<'/docs/[[...slug]]'>): P
     openGraph: {
       images: getPageImage(page).url,
     },
-  };
+  }
 }
