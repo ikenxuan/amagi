184343c887aa5c7b70f1d48ce28e5023675d7eaf
炫炫 <2066855608@qq.com>
2026-05-27 15:32:33 +0800

    chore: 接口响应类型更新

--- packages/core/src/types/ReturnDataType/Bilibili/Dynamic/DYNAMIC_TYPE_FORWARD/Forward/DYNAMIC_TYPE_AV/index.ts
+++ packages/core/src/types/ReturnDataType/Bilibili/Dynamic/DYNAMIC_TYPE_FORWARD/Forward/DYNAMIC_TYPE_AV/index.ts
@@ -1,3 +1,4 @@
 import { DynamicTypeAV_V0 } from './DYNAMIC_TYPE_AV_V0'
+import { DynamicTypeAV_V1 } from './DYNAMIC_TYPE_AV_V1'
 
-export type DynamicTypeAV = DynamicTypeAV_V0
+export type DynamicTypeAV = DynamicTypeAV_V0 | DynamicTypeAV_V1
